ff93fa999e2e4cbb9fca9e1b8042e0b38fea8e2f
Pablo <pcanogo19@gmail.com>
2018-04-22 15:30:10 -0400

    header

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,12 +5,53 @@ $( document ).ready(function() {
   	if($(document).scrollTop()>100) {
   		$("nav").removeClass("navbar-large");
   		$("nav").addClass("navbar-small");
+        $(".navbar-nav>li>a").addClass("nav-li-small");
+        $(".logo").addClass("logo-small");
+        $(".navbar-brand").css("padding-top", "12px");
   	} else {
   		$("nav").addClass("navbar-large");
         $("nav").removeClass("navbar-small");
-  		
+  		$(".navbar-nav>li>a").removeClass("nav-li-small");
+        $(".logo").removeClass("logo-small");
+        $(".navbar-brand").css("padding-top", "15px");
   	}
 
   });
+    
+    $.fn.extend({
+      animateCss: function(animationName, callback) {
+        var animationEnd = (function(el) {
+          var animations = {
+            animation: 'animationend',
+            OAnimation: 'oAnimationEnd',
+            MozAnimation: 'mozAnimationEnd',
+            WebkitAnimation: 'webkitAnimationEnd',
+          };
+
+          for (var t in animations) {
+            if (el.style[t] !== undefined) {
+              return animations[t];
+            }
+          }
+        })(document.createElement('div'));
+
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+          $(this).removeClass('animated ' + animationName);
+
+          if (typeof callback === 'function') callback();
+        });
+
+        return this;
+      },
+    });
+    
+    $('.header-logo').animateCss('rotateIn', function() {
+        $('.header-title').toggleClass('invisible');
+        $('.header-title').animateCss('bounceInDown', function() {
+            $('.title').toggleClass('invisible');
+            $('.title').animateCss('fadeIn', function() {
+            }); 
+        }); 
+    });
 
 });
